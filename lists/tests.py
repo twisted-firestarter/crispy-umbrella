d16e0787b65d76fb407810cb306e3c633c2e5d31
Szymon Sabina <szymon.sabina@hotmail.com>
2021-11-21 20:43:47 +0100

    Started the new app and built stub of first view and unit test for it

---
+++ lists/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import resolve
+
+from .views import home_page
+
+# Create your tests here.
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_homepage_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+        self.assertIs(found.func, home_page)
