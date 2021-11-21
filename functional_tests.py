8e23211ade433ed63e7818a82700c9bd04c17ac5
Szymon Sabina <szymon.sabina@hotmail.com>
2021-11-21 19:17:32 +0100

    Migrated functional_tests.py to unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'succes' in browser.title
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self) -> None:
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
