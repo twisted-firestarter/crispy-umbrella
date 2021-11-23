a1ea948e6530479f27b4de92bdd270aee5ba4b7c
Szymon Sabina <szymon.sabina@hotmail.com>
2021-11-23 19:46:04 +0100

    Refactor homepage view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import time
 import unittest
+
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -13,6 +17,24 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        new_item_input = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            new_item_input.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        new_item_input.send_keys('Buy peacock feathers')
+        new_item_input.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
 
 if __name__ == '__main__':
--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,9 @@
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
 
 from .views import home_page
-
-# Create your tests here.
 
 
 class HomePageTest(TestCase):
@@ -13,11 +12,6 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertIs(found.func, home_page)
 
-
     def test_homepage_returns_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do Lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
